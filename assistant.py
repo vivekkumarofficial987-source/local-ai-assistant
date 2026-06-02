3b11c1adc07755d4404bfaa28ef388beaab472ad
Vivek Kumar <vivekkumarofficial987@gmail.com>
2026-06-02 07:15:40 +0530

    basic memory and tools calls

--- assistant.py
+++ assistant.py
@@ -1,6 +1,15 @@
+
+import json
+from tools import get_time
+
 from ollama import chat
 
-messages=[
+try :
+    with open("memeory.json","r") as file :
+        messages =json.load(file)
+except:
+
+    messages=[
     {
         "role":"system",
         'content': 'You are an expert Python teacher. Explain concepts simply and give examples.'
@@ -8,8 +17,10 @@ messages=[
     }
      
 ]
-
+ 
+ 
 while True:
+   
     user_input = input("you:")
 
     if user_input.lower()=="exit":
@@ -21,21 +32,52 @@ while True:
             "content":user_input
         }
     )
-    print(messages)
+
+    
+
+    if user_input.startswith("add"):
+        numbers = user_input.split()
+
+        a = int(numbers[1])
+        b = int(numbers[2])
+
+        print("AI:", a + b)
+        continue
+
+    
+    if user_input.lower() == "time":
+        print("AI:", get_time())
+        continue
+
+
     
     
-    response = chat(
+    
+    stream = chat(
         model="llama3.2",
-        messages=messages
+        messages=messages,
+        stream=True
     )
 
     
-    answer=response["message"]["content"]
+    answer= ""
 
-    print("AI:",answer)
+    print("AI: ", end="")
+
+    for chunk in stream:
+        content=chunk["message"]["content"]
+        answer+=content
+        print(content ,end="",flush=True)
+
+    print()
 
     messages.append({
             "role" : "assistant",
             "content" :answer})
+    
+    with open("memory.json", "w") as file:
+        json.dump(messages, file, indent=4)
+
+    
 
 
---
+++ tools.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+def get_time():
+    return datetime.now().strftime("%H:%M:%S")
